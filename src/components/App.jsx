cffa0ee01764db4d3a015310b61a157050c52850
ZEvans1 <zte.zachary@gmail.com>
2018-06-25 13:33:02 -0700

    Create ticket and ticket list component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App() {
     return (
         <div>
             <Header/>
             <h2>{new Date().toLocaleTimeString()}</h2>
-            <h3>3a</h3>
-            <h3>Amy and Brigitte</h3>
-            <p><em>Just checking in.</em></p>
-            <hr/>
+            <TicketList />
         </div>
     );
 }
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket(){
+    return (
+        <div>
+            <h3>3a</h3>
+            <h3>Amy and Brigitte</h3>
+            <p><em>Just checking in.</em></p>
+            <hr/>
+        </div>
+    );
+}
+
+export default Ticket;
