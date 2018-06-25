654d7f3568e9239fe28b09cb5d3b9671cd187b8f
ZEvans1 <zte.zachary@gmail.com>
2018-06-24 23:25:21 -0700

    Implement live clock.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
 
-ReactDOM.render(
-    greeting,
-    document.getElementById('react-app-root')
-);
+function liveTime() {
+    var greeting = React.createElement('h1', {}, 'Hello, World!');
+    var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+    var app = React.createElement('div', {}, greeting, clock);
+
+    ReactDOM.render(
+        app,
+        document.getElementById('react-app-root')
+    );
+}
+
+setInterval(liveTime, 1000);
