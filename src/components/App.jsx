2179d8260a61f94e2812330f1992fa7e85c7332e
ZEvans1 <zte.zachary@gmail.com>
2018-06-25 13:28:35 -0700

    Create header component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
     return (
         <div>
-            <h1>Help Queue</h1>
+            <Header/>
             <h2>{new Date().toLocaleTimeString()}</h2>
             <h3>3a</h3>
             <h3>Amy and Brigitte</h3>
