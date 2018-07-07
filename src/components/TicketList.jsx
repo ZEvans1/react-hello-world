27e7b651dd27573037bbacaa5060cb4cc4a4ac52
ZEvans1 <zte.zachary@gmail.com>
2018-07-06 17:28:14 -0700

    Add in several tickets.

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+var masterTicketList = [
+  {
+    names: 'Amy and Brigitte',
+    location: '3A',
+    issue: 'Just checking in'
+  },
+  {
+    names: 'Chloe and Diana',
+    location: '3B',
+    issue: 'Just checking in too'
+  },
+  {
+    names: 'Ellen and Fiona',
+    location: '5A',
+    issue: 'Just checking in as well'
+  }
+];
+
 function TicketList(){
   return (
-    <Ticket 
-      location="3A"
-      names="Amy and Brigitte"
-      issue="Just checking in."/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) => 
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
+    </div>
   );
 }
 
